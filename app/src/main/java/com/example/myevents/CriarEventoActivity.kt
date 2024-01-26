3860f396bced6638ecd13ee41dc2036e54bb208e
Alimamy <135225940+alimam22@users.noreply.github.com>
2024-01-26 17:56:45 -0100

    ii

--- app/src/main/java/com/example/myevents/CriarEventoActivity.kt
+++ app/src/main/java/com/example/myevents/CriarEventoActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class CriarEventoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        /// hhhhhhhh  kkkk
+        /// hhhhhhhh  kkkkhh
         setContentView(R.layout.activity_criar_evento)
     }
 }
